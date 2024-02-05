48ea393d48a9a2cddc484ac1ac8257206449204c
MD Nazmul Hasan <mdnazmulhasanniloy323@gmail.com>
2024-02-05 16:46:24 +0600

    husky test

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,18 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
 
-const app: Application = express();
+const app: Application = express()
 
-app.use(cors());
+app.use(cors())
 
 //parser
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 //testing route
-app.get("/", (req: Request, res: Response) => {
-  res.send("working successfully");
-});
+app.get('/', (req: Request, res: Response) => {
+  ff
+  res.send('working successfully')
+})
 
-export default app;
+export default app
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,8 @@
-import dotenv from "dotenv";
-import Path from "path";
-dotenv.config({ path: Path.join(process.cwd(), ".env") });
+import dotenv from 'dotenv'
+import Path from 'path'
+dotenv.config({ path: Path.join(process.cwd(), '.env') })
 
 export default {
   port: process.env.PORT || 3000,
-  mongoURI: process.env.DB_URL || "mongodb://localhost:27017/test",
-};
+  mongoURI: process.env.DB_URL || 'mongodb://localhost:27017/test',
+}
