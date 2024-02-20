6b60e4b0fb847585bc3918989ec8a9f5e17c9e03
MD Nazmul Hasan <mdnazmulhasanniloy323@gmail.com>
2024-02-21 00:02:41 +0600

    [Done] unhandled Rejection detected

--- src/server.ts
+++ src/server.ts
@@ -2,17 +2,45 @@ import mongoose from 'mongoose'
 import config from './config/index'
 import app from './app'
 import { errorLogger, logger } from './shared/logger'
+import { Server } from 'http'
 
 async function main() {
+  let server: Server
   try {
     await mongoose.connect(config?.mongo_uri)
     logger.info(`database connection successful`)
-    app.listen(config?.port, () => {
+    server = app.listen(config?.port, () => {
       logger.info(`server started on port ${config?.port}`)
     })
   } catch (error) {
     errorLogger.error(`failed to connect database`, error)
   }
+
+  // process.on('unhandledRejection', error => {
+  //   console.log(`Unhandled rejection detected: ${error}`)
+
+  //   // Log the error before attempting to close the server
+  //   errorLogger.error(error)
+
+  //   // Close the server gracefully
+  //   server.close(() => {
+  //     console.log('Server closed')
+  //     process.exit(1)
+  //   })
+  // })
+
+  //unhandled Rejection detected
+  process.on('unhandledRejection', error => {
+    console.log(`Unhandled rejection detected: ${error}`)
+    if (server) {
+      server.close(() => {
+        errorLogger.error(error)
+        process.exit(1)
+      })
+    } else {
+      process.exit(1)
+    }
+  })
 }
 
 main()
