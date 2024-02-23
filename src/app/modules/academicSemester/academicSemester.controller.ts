6229ebd51cf2e75615913c67bee83af658119685
MD Nazmul Hasan <mdnazmulhasanniloy323@gmail.com>
2024-02-24 00:32:30 +0600

    [Done] optimized all controllers and specific partan for send response

--- src/app/modules/academicSemester/academicSemester.controller.ts
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -1,24 +1,28 @@
-import { RequestHandler } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { AcademicSemesterService } from './academicSemester.service';
 import ApiError from '../../../errors/api.errors';
+import CatchAsync from '../../../shared/catchAsync';
+import sendResponse from '../../../shared/sendResponse';
+import httpStatus from 'http-status';
 
-const semesterCreate: RequestHandler = async (req, res, next) => {
-  try {
+const semesterCreate = CatchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
     const data = req.body;
     const result = await AcademicSemesterService?.createSemester(data);
 
     if (!result) {
       throw new ApiError(400, 'academic semester creation failed');
     }
-    res.status(200).json({
+    next();
+
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
       success: true,
       message: 'academic semester created successfully!',
       data: result,
     });
-  } catch (error) {
-    next(error);
-  }
-};
+  },
+);
 
 export const AcademicSemesterController = {
   semesterCreate,
